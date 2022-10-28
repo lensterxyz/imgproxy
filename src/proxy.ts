363159683f828435a33f5887777ff55da03d1675
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-28 22:10:10 +0530

    chore: update status code

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,6 +6,14 @@ const router = express.Router();
 const apiProxy = createProxyMiddleware({
   target: "https://ik.imagekit.io/lensterimg",
   changeOrigin: true,
+  onProxyRes: (proxyRes) => {
+    proxyRes.statusCode = 200;
+    delete proxyRes.headers["x-server"];
+    delete proxyRes.headers["x-cache"];
+    delete proxyRes.headers["x-amz-cf-pop"];
+    delete proxyRes.headers["x-amz-cf-id"];
+    proxyRes.headers["x-server"] = "lenster imgproxy";
+  },
 });
 
 router.get("/*", apiProxy);
