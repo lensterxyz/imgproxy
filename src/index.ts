8388b723b8aff16f078442d2e8fbfc2ec0c2c7b6
Bigint <69431456+bigint@users.noreply.github.com>
2022-10-28 21:56:46 +0530

    chore: update port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import proxy from "./proxy";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use("/", proxy);
 
